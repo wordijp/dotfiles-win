e338d0ab74510b116d8250d7c3ce9affd2131882
wordi <wordijp@gmail.com>
2018-10-19 17:19:48 +0900

    add comment

--- .clink/scripts/clink.lua
+++ .clink/scripts/clink.lua
@@ -2,6 +2,8 @@
 local clink_lua_file = clink.get_env('CLINK_ROOT')..'/clink.lua'
 dofile(clink_lua_file)
 
+--- NOTE: 現在のgitブランチ状態を表示したいならcmderのclink.luaが参考になる
+
 local completions_dir = clink.get_env('USERPROFILE')..'/.clink/scripts/clink-completions/'
 for _,lua_module in ipairs(clink.find_files(completions_dir..'*.lua')) do
     -- Skip files that starts with _. This could be useful if some files should be ignored
